cd647475406883f8f207ef3799074b0171b8f408
LGH-remake <141968023+LGH-remake@users.noreply.github.com>
2023-08-11 00:28:35 +0200

    Add files via upload

---
+++ Vorlagen/Vorlage.js
@@ -0,0 +1,10 @@
+//window.addEventListener('scroll', function () {
+//    var navbar = document.getElementById('navbar');
+//    var scrolled = window.scrollY;
+//
+//    if (scrolled > 0) {
+//        navbar.classList.add('fixed-navbar');
+//    } else {
+//        navbar.classList.remove('fixed-navbar');
+//    }
+//});
